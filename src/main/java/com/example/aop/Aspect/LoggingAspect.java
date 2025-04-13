5aa7af82755d862ae60acd107fe850684326ab80
mdswaley <mdswaley075@gmail.com>
2025-04-13 22:05:34 +0530

    Logging test using aspect using execution pointcut

--- src/main/java/com/example/aop/Aspect/LoggingAspect.java
+++ src/main/java/com/example/aop/Aspect/LoggingAspect.java
@@ -15,4 +15,20 @@ public class LoggingAspect {
     public void beforeShipmentServiceMethod(JoinPoint jp) {
         log.info("Before method call {}", jp.getSignature());
     }
+
+    @Before("execution(* orderPackage(..))")
+    public void beforeAnyOrderPackageCall(){
+        log.info("Before any order package call logging.");
+    }
+
+    @Before("execution(* com.example.aop.Services.Imp.ShipmentImp.orderPackage(..))")
+    public void beforeInShipmentOrderPackageCall(){
+        log.info("Before in shipment order package call logging.");
+    }
+
+    @Before("execution(* com.example.aop.Services.Imp.*.*(..))")
+    public void beforeAnyMethodCall(JoinPoint joinPoint){
+        log.info("Before any method call logging kind {}",joinPoint.getKind());
+        log.info("Before any method call logging signature {}",joinPoint.getSignature());
+    }
 }
